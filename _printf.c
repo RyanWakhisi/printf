0d83cc66d3fe11f0466432aa1f24367fa5958c68
LennyKariuki <kariukileonard20@gmail.com>
2023-08-22 13:37:10 -0700

    Updated code

--- _printf.c
+++ _printf.c
@@ -1,67 +1,42 @@
 #include "main.h"
 
-void print_buffer(char buffer[], int *buff_ind);
-
 /**
  * _print - Printf function
- * @format: format.
- * Return: The printed chars
+ * @format: The args to the function.
+ * Return: The printed chars.
  *
  */
+
 int _printf(const char *format, ...);
 {
-	int i, printed = 0, printed_chars = 0;
-	int flags, width, precision, size, buff_ind = 0;
-	va_list list;
-	char buffer[BUFF_SIZE];
+	int count = 0;
+	va_list args;
 
 	if (format == NULL)
 		return (-1);
 
-	va_start(list, formart);
-
-	for (i = 0; format && format[i] != '\0'; i++)
+	va_start(args_list, format);
+	while (*format)
 	{
-		if (format[i] != '%')
+		if (*format == '%')
 		{
-			buffer[buff_ind++] = format[i];
-			if (buff_ind == BUFF_SIZE)
-				print_buffer(buffer, &buff_ind);
-			/* write(1, &format[i], 1);*/
-			printed_chars++;
-		}
+			format++;
+			if (*format == '\0' || *format == ' ')
+				break;
+
+			if (*format == 'c')
+				count += handle_c(args_list);
+			else if (*format == 's')
+				count += handle_s(args_list);
+			else if (*format == '%')
+				count += print_char('%');
+			 i
 		else
 		{
-			print_buffer(buffer, &buff_ind);
-			flags = get_flags(format, &i);
-			width = get_width(format, &i, list);
-			precision = get_precisio(format, &i list);
-			size = get_size(format, &i);
-			++i;
-			printed = handle_print(format, &i, list, buffer,
-					flags, width, precision, size);
-			if (printed == -1)
-				return (-1);
-			printed_char += printed;
+			count += print_char(*format);
 		}
+		format++;
 	}
-
-	print_buffer(buffe, &buff_ind);
-
-	va_end(list);
-
-	return (printed_chars);
-}
-
-/**
- * print_buffer - prints buffer size if it exists
- * @buffer: Array of chars
- * @buff_ind: Index at which to add next char, represents the length
- */
-void print_buffer(char buffer[], int *buff_ind)
-{
-	if (*buff_ind > 0)
-		write(1, &buffer[0], *buff_ind);
-
-	*buff_ind = 0;
+	va_end(args_list;
+	return (count);
 }
